3e1b39fddb2a91265a431fdeb34080ce45e60418
Akmongkol <orbit6142.com>
2024-09-18 16:29:40 +0700

    update-page-notfound

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Map from './components/Map'
+import Notfound from './Notfound';
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Map />} />
-    </Routes>
-  </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Map />} />
+        {/* Catch-all route for 404 */}
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
