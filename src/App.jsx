f38bd403a17e12cdcaa6f274f71fcb9c627d119c
Akmongkol <orbit6142.com>
2024-09-18 15:33:23 +0700

    fix-route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Map from './components/Map'
-import NotFound from './NotFound';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Map />} />
-      <Route path="*" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
   )
