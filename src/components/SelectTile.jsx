5a1dc28a369d6ac622101860e7c3dde594ef5ba6
Teelek <orbit6142@gmail.com>
2024-10-15 11:19:38 +0700

    update-selecttile

--- src/components/SelectTile.jsx
+++ src/components/SelectTile.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
 import { SpeedDial, SpeedDialAction } from '@mui/material';
 import SpeedDialIcon from '@mui/material/SpeedDialIcon';
 import WaterDropIcon from '@mui/icons-material/WaterDrop';
@@ -6,67 +7,99 @@ import AirIcon from '@mui/icons-material/Air';
 import ThermostatIcon from '@mui/icons-material/Thermostat';
 import AcUnitIcon from '@mui/icons-material/AcUnit';
 import RadarIcon from '@mui/icons-material/Radar';
-import { useTheme } from '@mui/material/styles';
-import ReactGA from 'react-ga4';  // Import ReactGA for event tracking
+import { useTheme, createTheme, ThemeProvider } from '@mui/material/styles';
+import ReactGA from 'react-ga4'; // Import ReactGA for event tracking
 
 function SelectTile({ onSelect }) {
-    const [open, setOpen] = useState(true);
-    const [activeAction, setActiveAction] = useState('p3h'); // Set the default active action to 'p3h'
+  const [open, setOpen] = useState(true);
+  const [activeAction, setActiveAction] = useState('p3h'); // Set the default active action to 'p3h'
 
+  const handleActionClick = (value) => {
+    setActiveAction(value);
+    onSelect(value);
 
-    const handleActionClick = (value) => {
-        setActiveAction(value);
-        onSelect(value);
-
-        ReactGA.event({
-            category: 'User',
-            action: value,
-        });
-    };
-
-    const theme = useTheme();
-    const getIconStyle = (action) => ({
-      color: activeAction === action ? theme.palette.primary.main : 'inherit',
+    ReactGA.event({
+      category: 'User',
+      action: value,
     });
+  };
 
-    return (
-        <div className="speed-dial-container">
-            <SpeedDial
-                ariaLabel="SpeedDial example"
-                icon={<SpeedDialIcon />}
-                direction="down"
-                open={open} // Keep actions open by default
-                onOpen={() => setOpen(true)}
-                onClose={() => setOpen(false)} // Keep it open on close attempt
-            >
-                <SpeedDialAction
-                    icon={<WaterDropIcon style={getIconStyle('p3h')} />}
-                    tooltipTitle="ปริมาณฝนสะสม 3 ชั่วโมง"
-                    onClick={() => handleActionClick('p3h')}
-                />
-                <SpeedDialAction
-                    icon={<AirIcon style={getIconStyle('mslp')} />}
-                    tooltipTitle="ความกดอากาศ"
-                    onClick={() => handleActionClick('mslp')}
-                />
-                <SpeedDialAction
-                    icon={<ThermostatIcon style={getIconStyle('t2m')} />}
-                    tooltipTitle="อุณหภูมิ"
-                    onClick={() => handleActionClick('t2m')}
-                />
-                <SpeedDialAction
-                    icon={<AcUnitIcon style={getIconStyle('rhum')} />}
-                    tooltipTitle="ความชื้น"
-                    onClick={() => handleActionClick('rhum')}
-                />
-                <SpeedDialAction
-                    icon={<RadarIcon style={getIconStyle('radar')} />}
-                    tooltipTitle="เรดาร์"
-                    onClick={() => handleActionClick('radar')}
-                />
-            </SpeedDial>
-        </div>
-    );
+  const theme = useTheme();
+  const getIconStyle = (action) => ({
+    color: activeAction === action ? theme.palette.primary.main : 'inherit',
+  });
+
+  // Create a custom theme to override the Tooltip styles
+  const customTheme = createTheme({
+    components: {
+      MuiTooltip: {
+        styleOverrides: {
+          tooltip: {
+            fontSize: '16px', // Increase font size
+            backgroundColor: theme.palette.grey[700], // Change background color
+            padding: '10px', // Add padding
+            maxWidth: 'none', // Allow tooltip to grow in width
+          },
+        },
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={customTheme}>
+      <Box
+        sx={{
+          position: 'absolute',
+          top: 10,
+          right: 10,
+          height: 330,
+          zIndex: 1000,
+          transform: 'translateZ(0px)',
+          flexGrow: 1,
+        }}
+      >
+        <SpeedDial
+          ariaLabel="SpeedDial example"
+          icon={<SpeedDialIcon />}
+          direction="down"
+          open={open} // Keep actions open by default
+          onOpen={() => setOpen(true)}
+          onClose={() => setOpen(false)} // Keep it open on close attempt
+        >
+          <SpeedDialAction
+            icon={<WaterDropIcon style={getIconStyle('p3h')} />}
+            tooltipTitle="ปริมาณฝนสะสม 3 ชั่วโมง"
+            title="ปริมาณฝนสะสม 3 ชั่วโมง"
+            onClick={() => handleActionClick('p3h')}
+          />
+          <SpeedDialAction
+            icon={<AirIcon style={getIconStyle('mslp')} />}
+            tooltipTitle="ความกดอากาศ"
+            title="ความกดอากาศ"
+            onClick={() => handleActionClick('mslp')}
+          />
+          <SpeedDialAction
+            icon={<ThermostatIcon style={getIconStyle('t2m')} />}
+            tooltipTitle="อุณหภูมิ"
+            title="อุณหภูมิ"
+            onClick={() => handleActionClick('t2m')}
+          />
+          <SpeedDialAction
+            icon={<AcUnitIcon style={getIconStyle('rhum')} />}
+            tooltipTitle="ความชื้น"
+            title="ความชื้น"
+            onClick={() => handleActionClick('rhum')}
+          />
+          <SpeedDialAction
+            icon={<RadarIcon style={getIconStyle('radar')} />}
+            tooltipTitle="เรดาร์"
+            title="เรดาร์"
+            onClick={() => handleActionClick('radar')}
+          />
+        </SpeedDial>
+      </Box>
+    </ThemeProvider>
+  );
 }
 
 export default SelectTile;
