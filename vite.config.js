e1d8c2e504b4ca4315526ddf92eb360e9a2e5d3b
Teelek <orbit6142@gmail.com>
2024-10-29 13:16:04 +0700

    update-code-thisday

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,15 @@ export default defineConfig({
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 15000,
+     // สามารถใช้การตั้งค่าเพิ่มเติมได้ เช่น การตั้งค่า output format
+     rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {  
+            return 'vendor'; // แยก vendor ไปเป็น chunk อื่น
+          }
+        },
+      },
+    },
   },
 })
